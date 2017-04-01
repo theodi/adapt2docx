257a94ce3eb6e162ca1c1688dd5a1a2b7fde6eee
Dave Tarrant <davetaz@theodi.org>
2017-04-01 12:19:46 +0100

    Updated with components file version

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-# Adapt2Docx
+# Adapt2Docx2 (uses components file)
+
+Convert adapt2 elearning pages into docx for easy client review.
+
+NOTE: It does not do the inverse! So you still have to apply changes by hand, or just use the authoring tool.
+
+## Requirements
+
+* Adapt 2 framework >2.0.11
+* php command line
+
+## Step 1 - Export each page you need as HTML (full page)
+
+Load each page your require as a docx in Google Chrome (not Safari).
+
+Once loaded export each as a full page (Web page, complete) into the course build directory.
+
+Once saved you can remove the files directory to just leave the HTML page.
+
+## Step 2 - Identify the identifiers marker (To be automated)
+
+On OS X or linux run (from your course directory)
+
+```cat languagefiles/en/export.json | grep \"id\"```
+
+If you have used the authoring tool there is a chance that all the IDs start the same, e.g. 58...
+
+## Step 3 - Run adapt2docx2
+
+Run
+
+```php adapt2docx2.php *inputfile.html* *identifer marker (e.g. 58)* > *outputfile.html*```
+
+## Step 5 - Use Convertio to convert the output files to docx (To be automated)
+
+Go to https://convertio.co/html-docx and upload the output html files from Step 4 to be converted.
+
+
+# Adapt2Docx (uses translator output)
 
 Convert adapt2 elearning pages into docx for easy client review.
 
---
+++ adapt2docx2.php
@@ -0,0 +1,145 @@
+<?php
+
+	error_reporting(E_ALL & ~E_NOTICE);
+
+	$file = $argv[1];
+	$id = $argv[2];
+	system('cat ' . $file . ' | grep ' . $id . ' > out.tmp');
+	$handle = fopen('out.tmp',"r");
+	while ($line = fgets($handle)) {
+		$line = trim($line);
+		$ids = explode(" " . $id, $line);
+		for($i=1;$i<count($ids);$i++) {
+			$oid = $id . substr($ids[$i],0,strpos($ids[$i]," "));
+			$oid = str_replace('"','',$oid);
+			$done[$oid] = true;
+			$oids[] = $oid;
+		}
+	}
+	fclose($handle);
+	unlink('out.tmp');
+	$ids = $oids;
+
+	$string = file_get_contents("../src/course/en/components.json");
+	$data = json_decode($string,true);
+	for ($i=0;$i<count($data);$i++) {
+		$node = $data[$i];
+		if ($node["_id"] && substr($node["_id"],0,strlen($id)) == $id) {
+			$parts[$node["_id"]] = $node;
+		}
+	}
+	for ($i=0;$i<count($ids);$i++) {
+		$component = $parts[$ids[$i]];
+		if ($component["_component"] != "graphic") {
+			output($component);
+			
+		}
+	}
+
+function output($component) {
+	if ($component["_component"] == "text" || $component["_component"] == "media") {
+		outputText($component);
+	} elseif ($component["_canShowFeedback"]) {
+		outputText($component);
+		outputQuestion($component);
+	} elseif ($component["_component"] == "accordion") {
+		outputText($component);
+		outputAccordion($component);
+	} elseif ($component["_component"] == "narrative") {
+		outputText($component);
+		outputNarrative($component);
+	} elseif ($component["_component"] == "matching") {
+		outputText($component);
+		outputMatching($component);
+	} elseif ($component) {
+		print_r($component);
+		exit(1);
+	}
+}
+
+function outputText($component) {
+	echo "<h2>" . strip_tags($component["displayTitle"]) . "</h2>";
+	echo "<p>" . trim($component["body"]) . "</p>";
+}
+function outputQuestion($component) {
+	$items = $component["_items"];
+	echo '<ul>';
+	for($i=0;$i<count($items);$i++) {
+		echo '<li>' . $items[$i]["text"] . '</li>';
+	}
+	echo '</ul>';
+	echo $component["_feedback"]["correct"];
+	echo $component["_feedback"]["_incorrect"]["final"];
+}
+function outputMatching($component) {
+	$items = $component["_items"];
+	echo '<ul>';
+	for($i=0;$i<count($items);$i++) {
+		echo '<li>' . $items[$i]["text"] . '</li>';
+		$options = $items[$i]["_options"];
+		echo '<ul>';
+		for ($j=0;$j<count($options);$j++) {
+			echo '<li>' . $options[$j]["text"] . '</li>';
+		}
+		echo '</ul>';
+	}
+	echo '</ul>';
+	echo $component["_feedback"]["correct"];
+	echo $component["_feedback"]["_incorrect"]["final"];
+}
+function outputAccordion($component) {
+	$items = $component["_items"];
+	for($i=0;$i<count($items);$i++) {
+		$item = $items[$i];
+		echo "<h3>" . strip_tags($item["title"]) . "</h3>";
+		echo "<p>" . $item["body"] . "</p>";
+	}
+}
+function outputNarrative($component) {
+	$items = $component["_items"];
+	for($i=0;$i<count($items);$i++) {
+		$item = $items[$i];
+		echo "<h3>" . strip_tags($item["title"]) . "</h3>";
+		echo "<p>" . $item["body"] . "</p>";
+	}
+}
+
+
+function reorder($data) {
+	$order = ['title','body','instruction'];
+	for($i=0;$i<count($order);$i++) {
+		$path = $order[$i];
+		for($j=0;$j<count($data);$j++) {
+			if ($data[$j]["path"] == $path) $ret[] = $data[$j];
+		}
+	}
+	for($j=0;$j<count($data);$j++) {
+		$path = $data[$j]["path"];
+		if (!in_array($path, $order)) {
+			$ret[] = $data[$j];
+		}
+	}
+	return $ret;
+}
+
+function filter($data) {
+	for($i=0;$i<count($data);$i++) {
+		$current = $data[$i];
+		if (trim($current["value"]) == "") {
+			continue;
+		}
+		if (trim($current["value"]) == "Component title") {
+			continue;
+		}
+		if (trim($current["value"]) == "Article title") {
+			continue;
+		}
+		if (trim($current["value"]) == "Page title") {
+			continue;
+		}
+		$ret[] = $data[$i];
+	}
+	return $ret;
+}
+
+?>
